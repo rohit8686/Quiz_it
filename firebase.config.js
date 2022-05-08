43a6563882bcbf5f3746995f28af2136536109e0
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-05-08 23:10:56 +0530

    feat : home page

---
+++ firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKep-FAdWjkTDFiJh57dKE-D3giKGzJTA",
+  authDomain: "quizit-6662c.firebaseapp.com",
+  projectId: "quizit-6662c",
+  storageBucket: "quizit-6662c.appspot.com",
+  messagingSenderId: "653278080445",
+  appId: "1:653278080445:web:d085a324d705b8042936d4",
+  measurementId: "G-V96MN4J0BZ",
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./frontend/Components/Navbar/Navbar";
+import { Routing } from "./frontend/Routing/Routing";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routing />
+    </>
+  );
+}
+
+export default App;
