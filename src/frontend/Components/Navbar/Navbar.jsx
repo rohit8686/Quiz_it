a03bab9502983d2403b4246ac8b5be489b59b567
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-05-16 23:00:25 +0530

    feat : user profile

--- src/frontend/Components/Navbar/Navbar.jsx
+++ src/frontend/Components/Navbar/Navbar.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/auth-context";
-import { toastContainer } from "../Toast/Toast";
 
 export const Navbar = () => {
-  const { currentUser, signout } = useAuth();
-  const navigate = useNavigate();
+  const { currentUser } = useAuth();
 
   return (
     <nav className="flex space-between p-1 nav">
@@ -17,16 +15,9 @@ export const Navbar = () => {
           <h3>Rules</h3>
         </Link>
         {currentUser ? (
-          <button
-            className="btn btn-error"
-            onClick={async () => {
-              await signout();
-              navigate("/", { replace: true });
-              toastContainer("Logged out", "error");
-            }}
-          >
-            Logout
-          </button>
+          <Link to="/profile" className="link">
+            <span className="material-icons-outlined">account_circle</span>
+          </Link>
         ) : (
           <Link to="/login" className="link">
             <button className="btn btn-error">Login</button>
---
+++ src/frontend/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toastContainer } from "../../Components/Toast/Toast";
+import { useAuth } from "../../contexts/auth-context";
+
+export const UserProfile = () => {
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+  const {
+    currentUser: { email, metadata },
+  } = useAuth();
+  const lastLogin = metadata.lastSignInTime;
+
+  return (
+    <div className="card card-width p-1">
+      <h2>Profile</h2>
+      <hr className="hr" />
+      <h3>
+        <strong>Email : </strong>
+        {email}
+      </h3>
+      <p className="pt-1">Last login : {lastLogin}</p>
+      <div className="pt-1">
+        <button
+          className="btn btn-error"
+          onClick={async () => {
+            await signout();
+            navigate("/", { replace: true });
+            toastContainer("Logged out", "error");
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
