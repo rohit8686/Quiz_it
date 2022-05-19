814f79353b9a594a7a5f1204890fe1c4cb4fa9e7
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-05-19 19:37:39 +0530

    fix : results, login, theme

--- src/frontend/Routing/PrivateRoute.jsx
+++ src/frontend/Routing/PrivateRoute.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useAuth } from "../contexts/auth-context";
 
 export const PrivateRoute = () => {
   const location = useLocation();
-  const { currentUser } = useAuth();
 
   return (
     <div>
-      {currentUser ? (
+      {JSON.parse(localStorage.getItem("user")) ? (
         <Outlet />
       ) : (
         <Navigate to="/login" state={{ from: location }} replace={true} />
