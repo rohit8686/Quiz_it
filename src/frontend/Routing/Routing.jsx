4ce8f877b3e04841ec79ace0d71e941c30ead9de
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-05-21 16:00:19 +0530

    fix : not found page

--- src/frontend/Routing/Routing.jsx
+++ src/frontend/Routing/Routing.jsx
@@ -19,13 +19,13 @@ export const Routing = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/rules" element={<Rules />} />
+      <Route path="*" element={<PageNotFound />} />
       <Route path="/" element={<PrivateRoute />}>
         <Route path="/quiz/:quizName" element={<Quiz />} />
         <Route path="/quiz/rules/:quizName" element={<Rules />} />
         <Route path="/results" element={<Results />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/leaderboard" element={<LeaderBoard />} />
-        <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
